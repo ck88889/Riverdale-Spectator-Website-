cd5542afdb48efac162e5d92f61addba00efc014
ck88889 <cait.kwan@gmail.com>
2022-12-31 01:47:16 -0500

    update js files

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,7 @@ let article_obj = [];
 var idx = 0; 
 var placeholder;
 
+//navigation search bar 
 function reset_search(){
     for(let i = 0; i < article_obj.length; i++){
         article_obj[i].remove();
@@ -40,8 +41,63 @@ function searchbar() {
     }
 }
 
+//carousel 
+let all_comics = []; 
+let all_horoscopes = [];  
+
+function viewmore(arr){
+    alert("I WAS CLICKED"); 
+}
+
+function get_current(arr){
+    current_idx = 0; 
+    //get index of current item that being shown to user 
+    for(let i = 0; i < arr.length; i++){
+        if(!arr[i].classList.contains("hide")){
+            current_idx = i; 
+        }
+    }
+    return current_idx; 
+}
+
+function carouselnext(arr){
+    current_idx = get_current(arr); 
+    //if you're @ the end of the carousel -> go back to the front
+    if(current_idx == arr.length -1){
+        arr[current_idx].classList.add("hide"); 
+        arr[0].classList.remove("hide"); 
+    }else{
+        arr[current_idx].classList.add("hide"); 
+        arr[current_idx + 1].classList.remove("hide"); 
+    }
+}
+
+function carouselback(arr){
+    current_idx = get_current(arr); 
+    //if you're @ the start of the carousel -> go to the back 
+    if(current_idx == 0){
+        arr[current_idx].classList.add("hide"); 
+        arr[arr.length -1].classList.remove("hide"); 
+    }else{
+        arr[current_idx].classList.add("hide"); 
+        arr[current_idx - 1].classList.remove("hide"); 
+    }
+}
+
+function init_show(arr){
+    for(let i = 1; i < arr.length; i++){
+        arr[i].classList.add("hide"); 
+    }
+}
+
+let all_news = [];
+
 window.onload = function(){
     document.getElementById("instagram").href = "#";
     document.getElementById("email").href = "mailto:hamilton.spect@gmail.com";
     document.getElementById("github").href = "https://github.com/ck88889/riverdale-spectator-website";
+
+    all_comics = (document.getElementById("comic")).querySelectorAll(".carousel_item");
+    init_show(all_comics);
+    all_horoscopes = (document.getElementById("horoscopes")).querySelectorAll(".carousel_item");
 }
