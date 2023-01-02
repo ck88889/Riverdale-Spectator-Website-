4c364d3a15c8ba52b6bbf60f6f717942af12af78
ck88889 <cait.kwan@gmail.com>
2023-01-01 21:48:37 -0500

    Update update_everything.py

--- website manager/update_everything.py
+++ website manager/update_everything.py
@@ -47,6 +47,9 @@ ignore_arr = ["ContentFile(path=\"all_links.txt\")", "ContentFile(path=\"js\")",
             "ContentFile(path=\"critic.html\")", "ContentFile(path=\"images\")", "ContentFile(path=\"js\")", "ContentFile(path=\"opinion.html\")" , 
             "ContentFile(path=\"riverdale spectator.css\")", "ContentFile(path=\"website manager\")", "ContentFile(path=\"news.html\")", "ContentFile(path=\"index.html\")"]
 
+other_arr = ["ContentFile(path=\"c&i.html\")", "ContentFile(path=\"critic.html\")", "ContentFile(path=\"opinion.html\")" , 
+             "ContentFile(path=\"news.html\")", "ContentFile(path=\"index.html\")"]
+
 articles_arr = repo.get_contents("")
 
 for i in range(len(articles_arr) - 1, -1, -1):
@@ -55,11 +58,13 @@ for i in range(len(articles_arr) - 1, -1, -1):
                     articles_arr.pop(i)
                     break
 
-for x in range(len(articles_arr)):
+for x in range(len(other_arr)):
     path = str(articles_arr[x]).replace("ContentFile(path=\"", "").replace("\")", "")
     filecontent = str(repo.get_contents(path).decoded_content.decode())
-    filecontent.replace(old, replacement)
+    arr_1 = filecontent.split("<!--website main pages links-->")
+    arr_2 = arr_1[1].split("<!--search bar-->")
 
-    formatted_content = BeautifulSoup(filecontent,'html.parser') #content to be formatted
-    update.update_file(path, formatted_content)
+    formatted_content = BeautifulSoup(arr_1[0] + "<!--website main pages links-->"
+        + replacement + "<!--search bar-->" + arr_2[1],'html.parser') #content to be formatted
+    update.update_file(path, formatted_content.prettify())
 
