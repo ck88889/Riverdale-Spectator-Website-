5f5973877fe9b8dd253134531c3803c4c195cdf3
ck88889 <cait.kwan@gmail.com>
2023-01-01 22:45:26 -0500

    update

--- website manager/update_everything.py
+++ website manager/update_everything.py
@@ -8,40 +8,7 @@ REPO = "ck88889/Riverdale-Spectator-Website-"
 g = Github(TOKEN) #access token - remeber to change 
 repo = g.get_repo(REPO) #access repostitory - remeber to change
 
-replacement = """<div class = "nav_links grid grid-cols-8">
-                <a class="hover:underline" href="index.html">
-                    HOME
-                </a>
-                <a class="hover:underline" href="news.html">
-                 NEWS
-                </a>
-                <a class="hover:underline" href="opinion.html">
-                 OPINION
-                </a>
-                <a class="hover:underline col-span-2" href="critic.html">
-                 CRITIC'S CORNER
-                </a>
-                <a class="hover:underline col-span-3" style = "text-align: left;" href="c&amp;i.html">
-                 CULTURE &amp; ILLUSTRATIONS
-                </a>
-            </div>"""
-old = """<div class = "nav_links grid grid-cols-5">
-                <a class="hover:underline" href="index.html">
-                    HOME
-                </a>
-                <a class="hover:underline" href="news.html">
-                 NEWS
-                </a>
-                <a class="hover:underline" href="opinion.html">
-                 OPINION
-                </a>
-                <a class="hover:underline" href="critic.html">
-                 CRITIC'S CORNER
-                </a>
-                <a class="hover:underline" href="c&amp;i.html">
-                 CULTURE &amp; ILLUSTRATIONS
-                </a>
-            </div>"""
+replacement = "<div class=\"logo\"><a href = \"index.html\"><img class=\"logo\" src=\"images/riverdale spectator logo crop.jpg\"/> </a></div>"
 
 ignore_arr = ["ContentFile(path=\"all_links.txt\")", "ContentFile(path=\"js\")", "ContentFile(path=\"c&i.html\")", 
             "ContentFile(path=\"critic.html\")", "ContentFile(path=\"images\")", "ContentFile(path=\"js\")", "ContentFile(path=\"opinion.html\")" , 
@@ -61,10 +28,10 @@ for i in range(len(articles_arr) - 1, -1, -1):
 for x in range(len(other_arr)):
     path = str(other_arr[x]).replace("ContentFile(path=\"", "").replace("\")", "")
     filecontent = str(repo.get_contents(path).decoded_content.decode())
-    arr_1 = filecontent.split("<!--website main pages links-->")
-    arr_2 = arr_1[1].split("<!--search bar-->")
+    arr_1 = filecontent.split("<!--logo image-->")
+    arr_2 = arr_1[1].split("<!--website main pages links-->")
 
-    formatted_content = BeautifulSoup(arr_1[0] + "<!--website main pages links-->"
-        + replacement + "<!--search bar-->" + arr_2[1],'html.parser') #content to be formatted
+    formatted_content = BeautifulSoup(arr_1[0] + "<!--logo image-->"
+        + replacement + "<!--website main pages links-->" + arr_2[1],'html.parser') #content to be formatted
     update.update_file(path, formatted_content.prettify())
 
